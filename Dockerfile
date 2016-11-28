a27246213c3fbb61f3384701f57f4ac3245a8daf
Sathish Babu <sathishbabur@hotmail.com>
2016-11-27 21:35:29 -0800

    Added Source

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
 FROM microsoft/dotnet-framework:4.6.2
-SHELL ["CMD"]
+SHELL ["powershell"]
 WORKDIR /msbuild
 COPY . .
+
+# Note: Get MSBuild 12.
+RUN Invoke-WebRequest "https://download.microsoft.com/download/9/B/B/9BB1309E-1A8F-4A47-A6C5-ECF76672A3B3/BuildTools_Full.exe" -OutFile "$env:TEMP\BuildTools_Full.exe" -UseBasicParsing  
+RUN &  "$env:TEMP\BuildTools_Full.exe" /Silent /Full  
+# Todo: delete the BuildTools_Full.exe file in this layer
+
+
+RUN SET "PATH=%PATH%;C:/'Program Files (x86)'/MSBuild/12.0/Bin/
+RUN C:/'Program Files (x86)'/MSBuild/12.0/Bin/msbuild.exe Demo.sln /t:Rebuild /p:Configuration=Release
+
 
 
 
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+
+namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Assembly Names");
+            foreach(string assemblyName in GetAssemblyNames())
+            {
+                Console.WriteLine(assemblyName);
+            }
+			
+        }
+        public static string[] GetAssemblyNames()
+        {
+            Assembly assembly = typeof(Program).Assembly;
+            AssemblyName[] referncedAssemblies = assembly.GetReferencedAssemblies();
+            string[] assemblies = new string[referncedAssemblies.Length];
+            int i = 0;
+            foreach (AssemblyName assemblyName in referncedAssemblies)
+            {
+                assemblies[i++] = assemblyName.FullName;
+            }
+            return assemblies;
+        }
+    }
+}
